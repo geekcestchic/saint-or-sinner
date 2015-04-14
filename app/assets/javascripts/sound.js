5b0bf54833c32b58407e7b31273efa45ecc2be56
Alex Howard <alexhoward007@hotmail.com>
2015-04-14 09:53:28 +0100

    changed images and audiofile sources to AWS

--- app/assets/javascripts/sound.js
+++ app/assets/javascripts/sound.js
@@ -23,27 +23,27 @@ $(document).ready(function(){
     // console.log('loaded sound');
     soundManager.createSound({
       id: 'hiccup', 
-      url:'/assets/hiccupdrunk.mp3', 
+      url:'https://s3-eu-west-1.amazonaws.com/saint-or-sinner/audiofiles/hiccupdrunk.mp3', 
     }); 
     soundManager.createSound({
       id: 'blower', 
-      url:'/assets/blower.wav', 
+      url:'https://s3-eu-west-1.amazonaws.com/saint-or-sinner/audiofiles/blower.wav', 
     }); 
     soundManager.createSound({
       id: 'ambulance', 
-      url:'/assets/ambulance.wav', 
+      url:'https://s3-eu-west-1.amazonaws.com/saint-or-sinner/audiofiles/ambulance.wav', 
     }); 
     soundManager.createSound({
       id: 'fail', 
-      url:'/assets/fail.mp3', 
+      url:'https://s3-eu-west-1.amazonaws.com/saint-or-sinner/audiofiles/fail.mp3', 
     }); 
     soundManager.createSound({
       id: 'ohno', 
-      url:'/assets/oh-no.wav', 
+      url:'https://s3-eu-west-1.amazonaws.com/saint-or-sinner/audiofiles/oh-no.wav', 
     }); 
     soundManager.createSound({
       id: 'fart', 
-      url:'/assets/fart.mp3', 
+      url:'https://s3-eu-west-1.amazonaws.com/saint-or-sinner/audiofiles/fart.mp3', 
     }); 
   } 
 
